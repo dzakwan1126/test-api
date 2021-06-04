7c1ab7d5740d57f1e2b44ff7e1a3ec1eeb3c38f0
dzakwa1126 <dzakwanmuhammad6@gmail.com>
2021-06-04 19:40:08 +0700

    Add route for user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,5 +22,10 @@ Route::post('/employee', [EmployeeController::class , 'create']);
 Route::put('/employee/{id}', [EmployeeController::class, 'update']);
 Route::delete('/employee/{id}', [EmployeeController::class, 'delete']);
 
+//Route untuk akses User
+Route::get('/user', [UserController::class, 'index'] );
+Route::post('/user', [UserController::class, 'create'] );
+Route::put('/user/{id}', [UserController::class, 'update'] );
+Route::delete('/user/{id}', [UserController::class, 'delete'] );
 
 
